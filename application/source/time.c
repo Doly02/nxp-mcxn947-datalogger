2163c9aadc14325cc4221e1eb9efca2c9f5edc55
Tomáš Dolák <tomas.dolak@outlook.cz>
2025-02-01 21:35:15 +0100

    [FIX/time] Fixes Setup of The Internal RTC (IRTC) Time.
    Fixes Setup of The Internal RTC (IRTC) Time Based on The Values From The External One.

--- application/source/time.c
+++ application/source/time.c
@@ -38,12 +38,22 @@ uint32_t LPI2C2_GetFreq(void)
     return CLOCK_GetLPFlexCommClkFreq(2u);
 }
 
+uint8_t ConvertTo24HourFormat(uint8_t hours)
+{
+	if (hours == 12) {
+		hours = 0;
+	}
+
+    return hours;
+}
+
 error_t TIME_InitIRTC(void)
 {
 	irtc_datetime_t time;
 	irtc_config_t irtcCfg;
 	RTC_time_t rtc_time;
 	RTC_date_t rtc_date;
+	status_t status = 2;
 
     /**
      * irtcCfg.wakeupSelect = true;
@@ -88,7 +98,7 @@ error_t TIME_InitIRTC(void)
 	RTC_GetTime(&rtc_time);
 	RTC_GetDate(&rtc_date);
 
-	time.day 	= (uint8_t) rtc_date.day;
+	time.day 	= (uint8_t) rtc_date.date;
 	time.month 	= (uint8_t) rtc_date.month;
 	time.year	= (uint16_t)(2000 + rtc_date.year);
 
@@ -96,18 +106,27 @@ error_t TIME_InitIRTC(void)
 	time.minute	= (uint8_t) rtc_time.min;
 	time.second = (uint8_t) rtc_time.sec;
 
+
 #if (true == DEBUG_ENABLED)
-	PRINTF("DEBUG: External RTC=%d/%d/%d %d:%d:%2d\r\n", rtc_date.year, rtc_date.month, rtc_date.day,
+	uint32_t year = 2000 + rtc_date.year;
+	PRINTF("DEBUG: External RTC=%d/%d/%d %d:%d:%2d\r\n", year, rtc_date.month, rtc_date.date,
 			rtc_time.hrs, rtc_time.min, rtc_time.sec);
 #endif /* (true == DEBUG_ENABLED) */
 
-    IRTC_SetDatetime(RTC, &time);
+    status = IRTC_SetDatetime(RTC, &time);
+    if (0 != status)
+    {
+    	return ERROR_IRTC;
+    }
 
+    // SDK_DelayAtLeastUs(3, SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY);
     IRTC_GetDatetime(RTC, &time);
+
+    uint32_t hrs = ConvertTo24HourFormat(time.hour);
 
 #if (true == DEBUG_ENABLED)
 	PRINTF("DEBUG: Internal RTC=%d/%d/%d %d:%d:%2d\r\n", time.year, time.month, time.day,
-			time.hour, time.minute, time.second);
+			hrs, time.minute, time.second);
 #endif /* (true == DEBUG_ENABLED) */
 
 	return ERROR_NONE;
